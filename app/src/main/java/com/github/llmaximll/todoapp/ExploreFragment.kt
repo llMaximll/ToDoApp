7b70d8654ee0eb63529da34643edcb32c3983c22
llMaximll <m.porunov123@mail.ru>
2022-01-04 17:59:13 +0500

    Добавлен набросок fragment_explore.xml

--- app/src/main/java/com/github/llmaximll/todoapp/ExploreFragment.kt
+++ app/src/main/java/com/github/llmaximll/todoapp/ExploreFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.github.llmaximll.todoapp.databinding.FragmentFirstBinding
+import com.github.llmaximll.todoapp.databinding.FragmentExploreBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ExploreFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentExploreBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +32,7 @@ class ExploreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 
     override fun onDestroyView() {
