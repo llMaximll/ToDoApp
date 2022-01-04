cb4a4f137bdc39e96278035edece5da7e30e913b
llMaximll <m.porunov123@mail.ru>
2022-01-04 16:55:05 +0500

    Добавлены необходимые зависимости

--- app/src/main/java/com/github/llmaximll/todoapp/ExploreFragment.kt
+++ app/src/main/java/com/github/llmaximll/todoapp/ExploreFragment.kt
@@ -11,7 +11,7 @@ import com.github.llmaximll.todoapp.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ExploreFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
