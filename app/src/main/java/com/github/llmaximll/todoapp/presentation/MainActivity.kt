718f6194df10825465929cd420e8c100c91d6855
llMaximll <m.porunov123@mail.ru>
2022-01-16 11:54:00 +0500

    Добавление в проект Firebase Crashlytics, Firebase Performance

--- app/src/main/java/com/github/llmaximll/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/github/llmaximll/todoapp/presentation/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.github.llmaximll.todoapp.presentation
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
